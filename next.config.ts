ddf638241e5b22a1f14e8ea5030734737d53e961
Wailakkad <akkadouail8@gmail.com>
2025-09-15 22:18:24 +0200

    we add domains: [i.pinimg.com], // allow loading images from Pinterest

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["i.pinimg.com"], // allow loading images from Pinterest
+  },
 };
 
 export default nextConfig;
